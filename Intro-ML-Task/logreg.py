fc039283dfb288c684f17971c5de4d182ef6b214
vsreyas <88322721+vsreyas@users.noreply.github.com>
2022-10-25 02:08:25 +0530

    Update logreg.py

--- Intro-ML-Task/logreg.py
+++ Intro-ML-Task/logreg.py
@@ -1,24 +1,21 @@
 from unittest import result
 import pandas as pd
 import numpy as np
+from mnist import MNIST
 
-train_df = pd.read_csv("/home/sirabas/MNIST/train.csv")
 test_df = pd.read_csv("/home/sirabas/MNIST/test.csv")
-X_train = train_df.drop('label',axis=1)
-y_train = train_df['label']
+data = MNIST('samples')
+
+X_train, y_train = data.load_training()
 X_test= test_df
 
-X_train = X_train/255
-X_test = X_test/255
-from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 
-knn = KNeighborsClassifier(n_neighbors=10)
-logreg = LogisticRegression(fit_intercept=True, multi_class='auto', penalty='l2', solver='sag',max_iter=1000,C=50)
+knn = KNeighborsClassifier(n_neighbors=7)
 
-logreg.fit(X_train, y_train)
+knn.fit(X_train, y_train)
 
-y_predict=logreg.predict(X_test)
+y_predict=knn.predict(X_test)
 
 image_id = pd.Series(range(1,28001),name='ImageId')
 y_preds = pd.Series(y_predict,name = 'Label')
