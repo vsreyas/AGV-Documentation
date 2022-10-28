1c5a1dca545ee5236c72612a6ef9eab74b50f2af
vsreyas <88322721+vsreyas@users.noreply.github.com>
2022-10-28 19:01:03 +0530

    Update logreg.py

--- Intro-ML-Task/logreg.py
+++ Intro-ML-Task/logreg.py
@@ -1,17 +1,17 @@
-from unittest import result
 import pandas as pd
 import numpy as np
-from mnist import MNIST
 
+train_df = pd.read_csv("/home/sirabas/MNIST/train.csv")
 test_df = pd.read_csv("/home/sirabas/MNIST/test.csv")
-data = MNIST('samples')
-
-X_train, y_train = data.load_training()
+X_train = train_df.drop('label',axis=1)
+y_train = train_df['label']
 X_test= test_df
+
+
 
 from sklearn.neighbors import KNeighborsClassifier
 
-knn = KNeighborsClassifier(n_neighbors=7)
+knn = KNeighborsClassifier(n_neighbors=3)
 
 knn.fit(X_train, y_train)
 
